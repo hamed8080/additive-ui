d58ea797eecda09e0a37cf29f2ee8c3077097d94
Hamed Hosseini <hamed8080@gmail.com>
2023-04-28 11:16:58 +0330

    - Add docc files

--- build+docc+commit.sh
+++ build+docc+commit.sh
@@ -3,7 +3,7 @@
 source $(dirname $0)/environment+variables.sh
 
 #Use git worktree to checkout the $DOCC_BRANCH_NAME branch of this repository in a $DOCC_BRANCH_NAME sub-directory
-# git worktree add --checkout $DOCC_BRANCH_NAME
+git worktree add --checkout $DOCC_BRANCH_NAME
 
 cd $DOCC_BRANCH_NAME #move to worktree directory to create all files there
 
@@ -46,19 +46,19 @@ CURRENT_COMMIT_HASH=$(git rev-parse --short HEAD)
 echo "worktree documentation path: ${PWD}/$DOCC_OUTPUT_FOLDER"
 git add $DOCC_OUTPUT_FOLDER
 
-# if [ -n "$(git status --porcelain)" ]; then
-#     echo "Documentation changes found. Committing the changes to the '$DOCC_BRANCH_NAME' branch."
-#     echo "Please call push manually"
-#     git commit -m "Update Github Pages documentation site to $CURRENT_COMMIT_HASH"
-#     open -n https://$GITHUB_USER_NAME.github.io/${HOST_BASE_PATH}/documentation/${LOWERCASE_TARGET_NAME}/
-# else
-#     # No changes found, nothing to commit.
-#     echo "No documentation changes found."
-# fi
+if [ -n "$(git status --porcelain)" ]; then
+    echo "Documentation changes found. Committing the changes to the '$DOCC_BRANCH_NAME' branch."
+    echo "Please call push manually"
+    git commit -m "Update Github Pages documentation site to $CURRENT_COMMIT_HASH"
+    open -n https://$GITHUB_USER_NAME.github.io/${HOST_BASE_PATH}/documentation/${LOWERCASE_TARGET_NAME}/
+else
+    # No changes found, nothing to commit.
+    echo "No documentation changes found."
+fi
 
-# git worktree remove -f $DOCC_BRANCH_NAME
+git worktree remove -f $DOCC_BRANCH_NAME
 
 # After deleting the worktree we should back to the root directory for pushing new files.
-# cd $ROOT_DIR
-# git push origin $DOCC_BRANCH_NAME
+cd $ROOT_DIR
+git push origin $DOCC_BRANCH_NAME
 
